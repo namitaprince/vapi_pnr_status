403a28dcafa599aedc9e9b09f10274904ef09fd8
namitaprince <namitaprince2017@gmail.com>
2025-04-15 17:35:34 +0530

    added cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const pnrRoutes = require("./routes/pnrRoutes");
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/api", pnrRoutes);
@@ -14,6 +16,8 @@ app.use("/api", pnrRoutes);
 app.get("/", (req, res) => {
   res.send(" Vapi Railway PNR Backend is running.");
 });
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
